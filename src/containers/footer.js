279d73c2a33dff3b8c9caebabf8fe279ddef6483
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-15 13:54:13 -0600

    added more columns to footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -10,6 +10,22 @@ export function FooterContainer() {
     <Footer.Row>
       <Footer.Column>
         <Footer.Link href="#">FAQs</Footer.Link>
+        <Footer.Link href="#">Investor Relations</Footer.Link>
+        <Footer.Link href="#">Ways to Watch</Footer.Link>
+        <Footer.Link href="#">Corporate Information</Footer.Link>
+        <Footer.Link href="#">Nateflix Originals</Footer.Link>
+      </Footer.Column>
+      <Footer.Column>
+        <Footer.Link href="#">Help Centre</Footer.Link>
+        <Footer.Link href="#">Jobs</Footer.Link>
+        <Footer.Link href="#">Terms of Use</Footer.Link>
+        <Footer.Link href="#">Contact Us</Footer.Link>
+      </Footer.Column>
+      <Footer.Column>
+        <Footer.Link href="#">Account</Footer.Link>
+        <Footer.Link href="#">Reddem gif</Footer.Link>
+        <Footer.Link href="#">Terms of Use</Footer.Link>
+        <Footer.Link href="#">Contact Us</Footer.Link>
       </Footer.Column>
     </Footer.Row>
     </Footer>
