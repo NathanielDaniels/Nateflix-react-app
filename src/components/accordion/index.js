49bc105d15d1bff5db49c0604848f1cf7750cc55
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-16 14:18:35 -0600

    working on Accordion for faqs section of footer

---
+++ src/components/accordion/index.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import {Container, Inner, Title, Header, Body} from '../accordion'
+
+export default function Accordion({children, ...restProps}) {
+  // const {isOpen, setIsOpen} = useState(false)
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  )
+}
+
+Accordion.Title = function AccordionTitle({children, ...restProps}) {
+  return <Title {...restProps}>{children}</Title>
+}
+
+Accordion.Header = function AccordionHeader({children, ...restProps}) {
+  return <Header {...restProps}>{children}</Header>
+}
+
+Accordion.Body = function AccordionBody({children, ...restProps}) {
+  return <Body {...restProps}>{children}</Body>
+}
---
+++ src/containers/faqs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Accordion } from '../components'
+
+export function Faqs() {
+  return (
+    <Accordion>
+      <Accordion.Title>Accordion Menu</Accordion.Title>
+    </Accordion>
+  )
+}
