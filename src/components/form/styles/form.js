1c6b6b84074e3250e68e5afc5b0af7d516227c09
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-24 10:29:35 -0600

    building form content for signin page

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,12 +1,37 @@
 import styled from 'styled-components';
 import { Link as ReachRouterLink } from 'react-router-dom';
 
-export const Container = styled.section``;
-export const Base = styled.form``;
-export const Title = styled.h2``;
-export const Text = styled.p``;
+export const Container = styled.section`
+  width: 500px;
+  margin: 0 auto;
+  background: rgba(0,0,0,.5);
+  padding: 20px;
+  border-radius: 10px;
+`;
+
+export const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h2`
+  color: red;
+  text-align: center;
+`;
+
+export const Text = styled.p`
+  color: #fff;
+`;
+
 export const TextSmall = styled.h4``;
+
 export const Link = styled(ReachRouterLink)``;
-export const Input = styled.input``;
+
+export const Input = styled.input`
+  margin: 10px 0;
+  padding: 5px;
+`;
+
 export const Error = styled.div``;
+
 export const Submit = styled.button``;
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,12 +4,40 @@ import { HeaderContainer } from '../containers/header';
 
 export default function Signin() {
   const [error, setError] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const handleSignin = event => {
+    event.preventDefault();
+    console.log("Email: ", emailAddress)
+    console.log("Password: ", userPassword)
+  }
+  
 
   return (
     <HeaderContainer>
       <Form>
         <Form.Title>Sign In</Form.Title>
         {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Text>Email</Form.Text>
+          <Form.Input 
+            type="email" 
+            placeholder="Email"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+            required
+          />
+          <Form.Text>Password</Form.Text>
+          <Form.Input 
+            type="password" 
+            placeholder="Password"
+            value={userPassword}
+            onChange={({ target }) => setUserPassword(target.value)}
+          />
+          <Form.Submit>Submit</Form.Submit>
+        </Form.Base>
       </Form>
     </HeaderContainer>
   )
