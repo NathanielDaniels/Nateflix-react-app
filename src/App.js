a5bb3507b7c743edfc5433df9fbc25b61ef157b7
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-24 10:07:33 -0600

    singin page setup and working.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
-import Home from './pages/Home'
-import Signin from './pages/signin'
+import { Home, Signin} from './pages'
 
 function App() {
   return (
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
 
 export default function Signin() {
   const [error, setError] = useState('');
