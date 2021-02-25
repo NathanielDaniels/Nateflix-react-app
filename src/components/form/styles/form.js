3cde867952e732b30543d6f37473225f139efcd2
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-25 14:02:49 -0600

    SignIn page is mostly complete minus some minor styling issues. Starting SignUp Page

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -2,41 +2,90 @@ import styled from 'styled-components';
 import { Link as ReachRouterLink } from 'react-router-dom';
 
 export const Container = styled.section`
-  width: 500px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 660px;
+  max-width: 450px;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 5px;
+  padding: 60px 68px 40px;
   margin: 0 auto;
-  background: rgba(0,0,0,.5);
-  padding: 20px;
-  border-radius: 10px;
+  margin-bottom: 100px;
 `;
 
 export const Base = styled.form`
   display: flex;
   flex-direction: column;
+  max-width: 450px;
+  width: 100%;
 `;
 
 export const Title = styled.h2`
-  color: red;
-  text-align: center;
+  color: #fff;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 28px;
 `;
 
 export const Text = styled.p`
-  color: #fff;
+  color: #737373;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
-export const TextSmall = styled.h4``;
+export const TextSmall = styled.h4`
+  margin-top: 10px;
+  font-size: 13px;
+  line-height: normal;
+  color: #8c8c8c;
+`;
 
-export const Link = styled(ReachRouterLink)``;
+export const Link = styled(ReachRouterLink)`
+  color: #fff;
+  text-decoration: none;
+  
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export const Input = styled.input`
-  margin: 10px 0;
-  padding: 5px;
+  background: #333;
+  border-radius: 4px;
+  border: 0;
+  color: #fff;
+  height: 50px;
+  line-height: 50px;
+  padding: 5px 20px;
+  margin-bottom: 20px;
+  
+  &:last-of-type {
+    margin-bottom: 30px;
+  }
 `;
 
-export const Error = styled.div``;
+export const Error = styled.div`
+  background: #e87c03;
+  border-radius: 4px;
+  font-size: 14px;
+  margin: 0 0 16px;
+  color: white;
+  padding: 15px 20px;
+`;
 
 export const Submit = styled.button`
-  width: 50%;
-  padding: 5px;
-  margin: 10px auto 0;
+  background: #e50914;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 12px;
+  padding: 16px;
+  border: 0;
+  color: white;
   cursor: pointer;
+  
+  &:disabled {
+      opacity: 0.5;
+  }
 `;
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,44 +1,63 @@
 import React, { useState } from 'react';
 import { Form } from '../components';
 import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
 
 export default function Signin() {
   const [error, setError] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [userPassword, setUserPassword] = useState('');
 
+  const isInvalid = userPassword === '' || emailAddress === '';
+
   const handleSignin = event => {
     event.preventDefault();
     console.log("Email: ", emailAddress)
     console.log("Password: ", userPassword)
-    
   }
   
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-        <Form.Base onSubmit={handleSignin} method="POST">
-          <Form.Text>Email</Form.Text>
-          <Form.Input 
-            type="email" 
-            placeholder="Email"
-            value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
-            required
-          />
-          <Form.Text>Password</Form.Text>
-          <Form.Input 
-            type="password" 
-            placeholder="Password"
-            value={userPassword}
-            onChange={({ target }) => setUserPassword(target.value)}
-          />
-          <Form.Submit>Submit</Form.Submit>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+          <Form.Base onSubmit={handleSignin} method="POST">
+            {/* <Form.Text>Email</Form.Text> */}
+            <Form.Input 
+              type="email" 
+              placeholder="Email"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+              required
+            />
+            {/* <Form.Text>Password</Form.Text> */}
+            <Form.Input 
+              type="password" 
+              placeholder="Password"
+              value={userPassword}
+              onChange={({ target }) => {
+                setUserPassword(target.value)
+                if (userPassword.split('').length = 0) {
+                  console.log('not valid password')
+                }
+              }}
+            />
+            <Form.Submit type="submit" disabled={isInvalid}>Sign In</Form.Submit>
+
+            <Form.Text>
+              New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   )
 }
