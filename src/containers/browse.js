bc783c7989a6d484cfcfe488141c4d1e05730aac
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-02-28 21:51:44 -0600

    architecting browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Header } from '../components'
+import * as ROUTES from '../constants/routes'
+import { FirebaseContext } from '../context/firebase'
+import { SelectProfileContainer } from './profiles'
+import { FooterContainer } from './footer'
+
 
 export function BrowseContainer() {
-    return (
-      <h1>Browse Container</h1>
-    );
+  const [profile, setProfile] = useState({});
+
+  const user = {
+    displayName: "Nathan",
+    photoURL: "1"
+  }
+     return profile.displayName ? (
+        <>
+            <p>Browse Container</p>
+            <FooterContainer />
+        </>)
+        : (<SelectProfileContainer />
+        );
 }
