7fbe1e0cbb9cb53925fc4a54fffc762e531757d4
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-03-06 16:09:10 -0600

    data saved to firebase successfully

--- src/index.js
+++ src/index.js
@@ -3,28 +3,27 @@ import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles'
 import './index.css';
 import App from './App';
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 import { FirebaseContext } from './context/firebase';
-import { firebase } from './lib/firebase.prod';
-// import Keys from './stash'
+// import { firebase } from './lib/firebase.prod';
+import Keys from './stash'
 
-// import { seedDatabase } from './seed';
-
-// const config = {
-//   apiKey: Keys().apiKey,
-//   projectId: Keys().projectId,
-//   authDomain: "netflix-clone-639dd.firebaseapp.com",
-//   databaseURL: "https://netflix-clone-639dd.firebaseio.com",
-//   storageBucket: "netflix-clone-639dd.appspot.com",
-//   messagingSenderId: "350534320019",
-//   appId: "1:350534320019:web:72808bf152cd772fb9f266",
-//   measurementId: "G-QPRM97YMQX"
-// };
-
-// const firebase = window.firebase.initializeApp(config);
+const config = {
+  apiKey: Keys().apiKey,
+  projectId: Keys().projectId,
+  authDomain: "netflix-clone-639dd.firebaseapp.com",
+  databaseURL: "https://netflix-clone-639dd.firebaseio.com",
+  storageBucket: "netflix-clone-639dd.appspot.com",
+  messagingSenderId: "350534320019",
+  appId: "1:350534320019:web:72808bf152cd772fb9f266",
+  measurementId: "G-QPRM97YMQX"
+};
+const firebase = Firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <FirebaseContext.Provider value={{ firebase: window.firebase }}> */}
     <FirebaseContext.Provider value={{ firebase }}>
       <GlobalStyles />
       <App />
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,26 +1,27 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import Keys from '../stash'
+// import Firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+// import Keys from '../stash'
 
-//? we need to seed the database
+//! we need to seed the database (!Do not use)
 // import { seedDatabase } from './seed';
+// seedDatabase(firebase);
 
 //? need a config
-const config = {
-  apiKey: Keys().apiKey,
-  projectId: Keys().projectId,
-  authDomain: "netflix-clone-639dd.firebaseapp.com",
-  databaseURL: "https://netflix-clone-639dd.firebaseio.com",
-  storageBucket: "netflix-clone-639dd.appspot.com",
-  messagingSenderId: "350534320019",
-  appId: "1:350534320019:web:72808bf152cd772fb9f266",
-  measurementId: "G-QPRM97YMQX"
-};
+// const config = {
+//   apiKey: Keys().apiKey,
+//   projectId: Keys().projectId,
+//   authDomain: "netflix-clone-639dd.firebaseapp.com",
+//   databaseURL: "https://netflix-clone-639dd.firebaseio.com",
+//   storageBucket: "netflix-clone-639dd.appspot.com",
+//   messagingSenderId: "350534320019",
+//   appId: "1:350534320019:web:72808bf152cd772fb9f266",
+//   measurementId: "G-QPRM97YMQX"
+// };
 
-const firebase = Firebase.initializeApp(config);
+// const firebase = Firebase.initializeApp(config);
 
-export { firebase };
+// export { firebase };
 
 //! Initialize Firebase
   // firebase.initializeApp(firebaseConfig);
