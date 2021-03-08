57ce51b2838e1c34a5edf021974d8a25a489e442
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-03-08 09:49:19 -0600

    adding redirect routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 import { Home, Browse, Signin, Signup} from './pages'
+import { IsUserRedirect } from './helpers/routes'
 
 function App() {
+  const user = {};
   return (
     <Router>
         <Switch>
-            <Route exact path={ROUTES.SIGN_IN}>
-                <Signin/>
-            </Route>
-            <Route path={ROUTES.SIGN_UP}>
+            <IsUserRedirect 
+              user={user} 
+              loggedInPath={ROUTES.BROWSE} 
+              path={ROUTES.SIGN_IN}
+              exact
+              >
+              <Signin/>
+            </IsUserRedirect>
+            <IsUserRedirect 
+              user={user} 
+              loggedInPath={ROUTES.BROWSE} 
+              path={ROUTES.SIGN_UP}
+              exact
+              >
+              <Signup/>
+            </IsUserRedirect>
+            {/* <Route exact path={ROUTES.SIGN_IN}>
+              <Signin/>
+            </Route> */}
+            {/* <Route path={ROUTES.SIGN_UP}>
                 <Signup/>
-            </Route>
+            </Route> */}
             <Route path={ROUTES.BROWSE}>
                 <Browse/>
             </Route>
