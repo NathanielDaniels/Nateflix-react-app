4f34fd4f1a0207ed44414bdd0a53efb515015acc
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-03-18 21:29:25 -0500

    working on bug with itemFeature hook not showing correct info.

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -30,8 +30,37 @@ Card.Text = function CardText({ children, ...restProps }) {
   return <Text { ...restProps }>{ children }</Text>
 }
 
+Card.Entities = function CardEntities({ children, ...restProps }) {
+  return <Entities { ...restProps }>{ children }</Entities>
+}
+
+Card.Meta = function CardMeta({ children, ...restProps }) {
+  return <Meta { ...restProps }>{ children }</Meta>
+}
+
+Card.Item = function CardItem({ children, ...restProps }) {
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+
+  return (
+    <Item 
+      onClick={() => {
+        setItemFeature(Item);
+        setShowFeature(true);
+      }}
+      { ...restProps }
+    >
+      { children }
+    </Item>
+  );
+};
+
+Card.Image = function CardImage({ ...restProps }) {
+  return <Image { ...restProps } />
+}
+
 Card.Feature = function CardFeature({ children, category, ...restProps }) {
   const { showFeature, itemFeature, setShowFeature} = useContext(FeatureContext);
+  console.log(itemFeature)
 
   return showFeature ? (
     <Feature src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`} { ...restProps }>
@@ -44,62 +73,62 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
 
          <Group margin="30px 0" flexDirection="row" alignItems="center">
           <Maturity rating={itemFeature.maturity}>
+            { console.log("Maturity Rating: ", itemFeature.maturity) }
             { itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity }
           </Maturity>
           <FeatureText fontWeight="600">
-            { itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1) }
+            {/* { console.log("genre", itemFeature.maturity) } */}
+            {/* { itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1) } */}
           </FeatureText>
         </Group>
         {children}
       </Content>
-
-     
     </Feature>
   ) : null;
 }
 
-Card.FeatureText = function CardFeatureText({ children, ...restProps }) {
-  return <FeatureText { ...restProps }>{ children }</FeatureText>
-}
+// Card.FeatureText = function CardFeatureText({ children, ...restProps }) {
+//   return <FeatureText { ...restProps }>{ children }</FeatureText>
+// }
 
-Card.FeatureClose = function CardFeatureClose({ children, ...restProps }) {
-  return <FeatureClose { ...restProps }>{ children }</FeatureClose>
-}
+// Card.FeatureClose = function CardFeatureClose({ children, ...restProps }) {
+//   return <FeatureClose { ...restProps }>{ children }</FeatureClose>
+// }
 
-Card.FeatureTitle = function CardFeatureTitle({ children, ...restProps }) {
-  return <FeatureTitle { ...restProps }>{ children }</FeatureTitle>
-}
+// Card.FeatureTitle = function CardFeatureTitle({ children, ...restProps }) {
+//   return <FeatureTitle { ...restProps }>{ children }</FeatureTitle>
+// }
 
-Card.Maturity = function CardMaturity({ children, ...restProps }) {
-  return <Maturity { ...restProps }>{ children }</Maturity>
-}
+// Card.Maturity = function CardMaturity({ children, rating, ...restProps }) {
+//   return <Maturity rating={rating} { ...restProps }>{ children }</Maturity>
+// }
 
-Card.Content = function CardContent({ children, ...restProps }) {
-  return <Content { ...restProps }>{ children }</Content>
-}
+// Card.Content = function CardContent({ children, ...restProps }) {
+//   return <Content { ...restProps }>{ children }</Content>
+// }
 
-Card.Entities = function CardEntities({ children, ...restProps }) {
-  return <Entities { ...restProps }>{ children }</Entities>
-}
+// Card.Entities = function CardEntities({ children, ...restProps }) {
+//   return <Entities { ...restProps }>{ children }</Entities>
+// }
 
-Card.Meta = function CardMeta({ children, ...restProps }) {
-  return <Meta { ...restProps }>{ children }</Meta>
-}
+// Card.Meta = function CardMeta({ children, ...restProps }) {
+//   return <Meta { ...restProps }>{ children }</Meta>
+// }
 
-Card.Item = function CardItem({ children, ...restProps }) {
-  const { setShowFeature, setItemFeature } = useContext(FeatureContext)
+// Card.Item = function CardItem({ children, ...restProps }) {
+//   const { setShowFeature, setItemFeature } = useContext(FeatureContext)
 
-  return (
-    <Item 
-      onClick={() => {
-        setItemFeature(Item);
-        setShowFeature(true);
-      }}
-      { ...restProps }>
-        { children }
-    </Item>)
-}
+//   return (
+//     <Item 
+//       onClick={() => {
+//         setItemFeature(Item);
+//         setShowFeature(true);
+//       }}
+//       { ...restProps }>
+//         { children }
+//     </Item>)
+// }
 
-Card.Image = function CardImage({ ...restProps }) {
-  return <Image { ...restProps } />
-}
+// Card.Image = function CardImage({ ...restProps }) {
+//   return <Image { ...restProps } />
+// }
--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -7,7 +7,16 @@ export const Container = styled.div`
 `;
 
 export const Group = styled.div`
+  display: flex;
+  flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
+  ${({ margin }) => margin && `margin: ${margin}`};
 
+  > ${Container}:first-of-type {
+    @media (min-width: 1100px) {
+      margin-top: -100px;
+    }
+  }
 `;
 
 export const Title = styled.h2`
@@ -17,11 +26,112 @@ export const Title = styled.h2`
 `;
 
 export const SubTitle = styled.h4`
-
+  font-size: 12px;
+  color: #fff;
+  font-weight: bold;
+  margin-top: 0;
+  margin-bottom: 0;
+  user-select: none;
+  display: none;
 `;
 
 export const Text = styled.p`
+  margin-top: 5px;
+  font-size: 10px;
+  color: #fff;
+  margin-bottom: 0;
+  user-select: none;
+  display: none;
+  line-height: normal;
+`;
 
+export const Entities = styled.div`
+  ${'' /* background: #fff; */}
+  display: flex;
+
+  ${'' /* overflow-x: scroll; */}
+  ${'' /* overflow-y: hidden; */}
+  ${'' /* -ms-overflow-style: none;}
+  ${'' /* scrollbar-width: none; */}
+`;
+
+export const Meta = styled.div`
+  width: 100%;
+  cursor: default;
+  ${'' /* z-index: 1000; */}
+  ${'' /* background: #fff; */}
+`;
+
+export const Image = styled.img`
+  width: fit-content;
+  ${'' /* width: 100%; */}
+  border: 0;
+  max-width: 305px;
+  cursor: pointer;
+  ${'' /* height: auto; */}
+  ${'' /* padding: 0; */}
+  ${'' /* margin: 0; */}
+`;
+
+export const Item = styled.div`
+  ${'' /* margin: 0 5px;
+  border: 2px solid black;
+  z-index: 100;
+  transition: transform .3s ease-in-out;
+  &:hover {
+    transform: scale(1.05);
+    z-index: 1000;
+  }
+  &:first-child {
+    margin-left: 15px;
+  } */}
+
+
+  display: flex;
+  flex-direction: column;
+  margin-right: 5px;
+  position: relative;
+  cursor: pointer;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.3);
+    z-index: 99;
+  }
+
+  @media (min-width: 1200px) {
+    &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
+      display: block;
+      z-index: 100;
+    }
+  }
+
+  &:first-of-type {
+    margin-left: 56px;
+
+    @media (max-width: 1000px) {
+      margin-left: 30px;
+    }
+  }
+
+  &:last-of-type {
+    margin-right: 56px;
+
+    @media (max-width: 1000px) {
+      margin-right: 30px;
+    }
+  }
+`;
+
+export const FeatureText = styled.p`
+  font-size: 18px;
+  color: #fff;
+  font-weight: ${({ fontWeight }) => (fontWeight === 'bold' ? 'bold' : 'normal')};
+  margin: 0;
+
+  @media (max-width: 600px) {
+    line-height: 22px;
+  }
 `;
 
 export const Feature = styled.div`
@@ -52,54 +162,50 @@ export const Feature = styled.div`
 `;
 
 export const FeatureTitle = styled(Title)`
-
+  margin-left: 0;
 `;
 
-export const FeatureText = styled.p`
 
-`;
 export const FeatureClose = styled.button`
-
-`;
-export const Maturity = styled.div`
-
-`;
-export const Content = styled.div`
-
-`;
-export const Meta = styled.div`
-  width: 100%;
-  cursor: default;
-  ${'' /* z-index: 1000; */}
-  ${'' /* background: #fff; */}
-`;
-export const Entities = styled.div`
-  ${'' /* background: #fff; */}
-  display: flex;
-
-  ${'' /* overflow-x: scroll; */}
-  ${'' /* overflow-y: hidden; */}
-  ${'' /* -ms-overflow-style: none;}
-  ${'' /* scrollbar-width: none; */}
-
-  
-`;
-export const Item = styled.div`
-  margin: 0 5px;
-  border: 2px solid black;
-  z-index: 100;
-  transition: transform .3s ease-in-out;
-  &:hover {
-    transform: scale(1.05);
-    z-index: 1000;
-  }
-  &:first-child {
-    margin-left: 15px;
-  }
-`;
-
-export const Image = styled.img`
+  color: white;
+  position: absolute;
+  right: 20px;
+  top: 20px;
   cursor: pointer;
-  ${'' /* height: 100%; */}
-  width: fit-content;
+  background-color: transparent;
+  border: 0;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 24px;
+  }
 `;
+
+export const Content = styled.div`
+  margin: 56px;
+  max-width: 500px;
+  line-height: normal;
+
+  @media (max-width: 1000px) {
+    margin: 30px;
+    max-width: none;
+  }
+`;
+
+export const Maturity = styled.div`
+  background-color: ${({ rating }) => (rating >= 15 ? '#f44336' : '#2f9600')};
+  border-radius: 15px;
+  width: 28px;
+  line-height: 28px;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  margin-right: 10px;
+  font-size: 12px;
+`;
+
+
+
+
+
