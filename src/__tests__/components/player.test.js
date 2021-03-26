c038e3b3da8aaa310878f83a1ebb5a4fcff09168
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-03-26 10:13:30 -0500

    added test folder

---
+++ src/__tests__/components/player.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Player } from '../../components';
+
+describe('<Player />', () => {
+  it('renders the <Player /> with a video', () => {
+    const { getByText, queryByTestId }
+  });
+});
