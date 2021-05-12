62a36ca21e40756a3412af614b87c957193d795d
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-05-12 00:35:28 -0500

    changed app to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import { GlobalStyles } from './global-styles'
 import './index.css';
-import App from './app';
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
